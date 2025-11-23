a8da7c833469af4538f2983f45c08a0fe393290b
Bruno Freitas <46687348+brunolf96@users.noreply.github.com>
2025-11-23 15:27:24 -0300

    Update Script_delete.sql
    atualização de comentários

--- Script_delete.sql
+++ Script_delete.sql
@@ -1,9 +1,12 @@
 ----- Remoção de dados nas Tabelas -----
+----- Remoção de um conjunto de materiais que não estão mais presentes na biblioteca
 DELETE FROM Material
 WHERE ID_Material IN (1, 2, 3);
 
+----- Remoção de um item do acervo que não tem mais exemplares presentes na biblioteca
 DELETE FROM Acervo
 WHERE ID_Acervo = 1;
 
+----- Remoção dos registros mais antigos
 DELETE FROM Consulta
 WHERE Data_da_consulta <= '2025-05-01';
